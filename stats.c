768ded0295f7e9ddf5ea98a3dd1c21199b4d978c
mohamedegila <mohammedegila@gmail.com>
2017-07-21 03:17:36 +0200

    adding code outline and documentation for c1m1 assessment

---
+++ stats.c
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file statistical analytics application 
+ * @brief This is a simple application that performs statistical analytics on adataset
+ *
+ * @author Mohammed Egila
+ * @date 21/7/2017
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+ 
+
+}
+
+void sort_array(unsigned char *ptr,int size) {
+/* this is function sort any passed array using buble sort algorithm*/
+   
+}
+
+void print_array(unsigned char *ptr,int size) {
+
+ 
+}
+
+unsigned char find_maximum(unsigned char *ptr,int size){
+ 
+}
+
+unsigned char find_minimum(unsigned char *ptr,int size){
+ 
+}
+
+unsigned char find_median(unsigned char *ptr,int size) {
+
+   
+}
+
+unsigned char find_mean(unsigned char *ptr,int size) {
+
+   }
+
+void print_statistics(unsigned char *ptr,int size) {
+    /*print all statistics by using printf function and calling every specific function in side it*/
+   
+}
